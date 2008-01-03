6b8528afef2a1045bd5c6007feb860ce77bfa5df
Dominic Hargreaves <dom@earth.li>
2008-01-03 22:42:02 +0000

    use common date

--- t/test.pl
+++ t/test.pl
@@ -8,7 +8,6 @@ plan tests => 4;
 use_ok( 'Oxford::Calendar' );
 require_ok( 'Oxford::Calendar' );
 
-is( Oxford::Calendar::ToOx(24,2,2002), "Sunday, 7th week, Hilary 2002." );
-is( Oxford::Calendar::FromOx(
-                    Oxford::Calendar::Parse("Sunday, 7th week, Hilary 2002")),
-                 "24/2/2002" );
+my $testdate1 = 'Sunday, 7th week, Hilary 2002.';
+is( Oxford::Calendar::ToOx(24,2,2002), $testdate1 );
+is( Oxford::Calendar::FromOx( Oxford::Calendar::Parse($testdate1)), "24/2/2002" );
